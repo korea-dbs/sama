b5289f19684060aa42a46a6222bf37dae9c31942
이도환 <71830104+lee0798@users.noreply.github.com>
2025-05-17 20:21:31 +0900

    Create x2.sql

---
+++ sambench/api/sources/queries/x2.sql
@@ -0,0 +1,1 @@
+SELECT _id, owner_package_name, is_pending, media_type, is_trashed FROM files WHERE ((volume_name IN ( 'external_primary' ))) AND ((_data='/storage/emulated/0/DCIM/batch-20240309-102430/006-xmp.jpeg'));
