c7172d4387c9508c52e06fc0f0e08564390f330b
이도환 <71830104+lee0798@users.noreply.github.com>
2025-05-30 16:13:37 +0900

    Delete sambench/api/sources/queries/q1-optimized.sql

--- sambench/api/sources/queries/q1-optimized.sql
+++
@@ -1,8 +0,0 @@
-select _id, _data, date_added, datetaken, mime_type, bucket_display_name, bucket_id, orientation FROM
-images where (is_trashed=0) AND
-    (volume_name in ('external_primary')) AND
-    ((_size>0)) AND
-    (((is_pending=0 AND not _modifier=4) OR
-      (is_pending=1 AND owner_package_name in ('com.campmobile.snow') AND
-       lower(_data) not regexp '/\.pending-(\d+)-([^/]+)$'))) ORDER BY datetaken DESC;
-
